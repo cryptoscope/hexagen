a9bbe5e40e9cb2f34344ae2572eecd7596cb60dd
Henry <cryptix@riseup.net>
2018-08-14 17:44:16 +0200

    better error message

--- generate.go
+++ generate.go
@@ -97,7 +97,7 @@ func Generate(id string, width float64) (*Grid, error) {
 	}
 
 	if idSplit[1] != "ed25519" && idSplit[1] != "sha256" {
-		return nil, errors.Errorf("hexagen: %q does not look like an id.", idSplit[1])
+		return nil, errors.Errorf("hexagen: %q is no known suffix", idSplit[1])
 	}
 
 	b64Key := strings.Split(id[1:], ".")[0]
